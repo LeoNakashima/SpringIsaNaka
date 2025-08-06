e7269ccfbaed26304ec447eb0f50e7e188be38ed
Leonardo Chen <leonardo.chen@picpay.com>
2025-08-06 16:21:37 -0300

    Adicionando o projeto Spring

---
+++ projIsaNaka/src/main/java/com/example/projisanaka/ProjIsaNakaApplication.java
@@ -0,0 +1,13 @@
+package com.example.projisanaka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjIsaNakaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjIsaNakaApplication.class, args);
+    }
+
+}
---
+++ projIsaNaka/src/test/java/com/example/projisanaka/ProjIsaNakaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projisanaka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjIsaNakaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
